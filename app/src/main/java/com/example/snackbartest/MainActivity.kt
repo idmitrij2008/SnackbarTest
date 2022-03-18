a35900705dc0e911d3d8367b919d5df0e7bc6bcb
dzhurba <idmitrij2008@yandex.ru>
2022-03-18 14:12:43 +0300

    2nd

--- app/src/main/java/com/example/snackbartest/MainActivity.kt
+++ app/src/main/java/com/example/snackbartest/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.snackbartest
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.snackbartest.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
+
+private const val NO_DRAWABLE = 0
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -14,10 +19,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.button.setOnClickListener {
-            Snackbar
-                .make(binding.root, "Text", Snackbar.LENGTH_SHORT)
-                .apply { anchorView = binding.button }
-                .show()
+            val snackBar = Snackbar.make(binding.root, "Text", Snackbar.LENGTH_SHORT).apply {
+                setBackgroundTint(ContextCompat.getColor(this@MainActivity, R.color.red_error))
+            }
+
+            snackBar.view.findViewById<TextView>(com.google.android.material.R.id.snackbar_text)
+                .run {
+                    setTextColor(ContextCompat.getColor(this@MainActivity, R.color.white))
+                    setCompoundDrawablesWithIntrinsicBounds(
+                        R.drawable.union,
+                        NO_DRAWABLE,
+                        NO_DRAWABLE,
+                        NO_DRAWABLE
+                    )
+                    compoundDrawablePadding = 16
+                    textAlignment = View.TEXT_ALIGNMENT_CENTER
+                }
+
+//            snackBar.anchorView = binding.textView
+
+            snackBar.show()
         }
     }
 }
